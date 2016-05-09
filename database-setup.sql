92b7085b53fed78e760f60f62a86ddbc3d6ad430
Gianfranco Cecconi <giacecco@dico.im>
2016-05-09 07:44:01 +0100

    Now recording the moment an RSS item is first downloaded

--- database-setup.sql
+++ database-setup.sql
@@ -1,6 +1,17 @@
 CREATE TABLE rss_read AS SELECT * FROM rss_item WHERE 0;
+
 ALTER TABLE rss_read ADD COLUMN item_last_modified TIMESTAMP DEFAULT NULL;
-CREATE TRIGGER update_record_last_modified AFTER UPDATE ON rss_item
+
+CREATE TRIGGER insert_item_last_modified AFTER INSERT ON rss_item
+    FOR EACH ROW
+    BEGIN
+        INSERT INTO rss_read
+            SELECT a.*, datetime('now', 'localtime') AS item_last_modified
+            FROM rss_item AS a
+            WHERE rowid = new.rowid;
+    END;
+
+CREATE TRIGGER update_item_last_modified AFTER UPDATE ON rss_item
     FOR EACH ROW
         WHEN (old.unread AND NOT new.unread) OR
              ((NOT old.deleted) AND new.deleted) OR
